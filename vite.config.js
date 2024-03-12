9b78cc07e26a72d4b2316e248cd54a94a515b78d
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2024-03-13 00:31:22 +0500

    add PORT

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_DOMAIN,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+      port: env.VITE_PORT
+    },
+  };
 });
-
